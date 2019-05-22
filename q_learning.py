f4752c4859b22acf21c67d91c2a0f19583547dcd
jacek257 <jacek257@gmail.com>
2019-05-22 10:35:10 -0700

    removed the behavior='onceonly' from RewardForCollectingItem

--- q_learning.py
+++ q_learning.py
@@ -462,7 +462,7 @@ def XML_generator(x,y,items):
                       </RewardForTouchingBlockType>
                       <RewardForSendingCommand reward="-1"/>
                       <RewardForCollectingItem>
-                        <Item reward="100.0" type="diamond"  behaviour="onceOnly"/>
+                        <Item reward="100.0" type="diamond" />
                       </RewardForCollectingItem>
                       <AgentQuitFromTouchingBlockType>
                           <Block type="lava" />
