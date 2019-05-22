d3af4a8807929c5eb21f0818da8ac7926ba09064
dannym08 <danielmardirousi@gmail.com>
2019-05-21 18:55:05 -0700

    Update tabular_q_learning.py
    Fixed iron_block (test collectable reward) to trigger only once

--- tabular_q_learning.py
+++ tabular_q_learning.py
@@ -439,7 +439,7 @@ def XML_generator(x,y):
                         <Block reward="-100.0" type="red_sandstone" behaviour="onceOnly"/>
                         <Block reward="-500.0" type="stone" behaviour="onceOnly"/>
                         <Block reward="-75.0" type="gold_block"/>
-						<Block reward="50" type="iron_block"/>
+						<Block reward="50" type="iron_block" behavior="onceOnly"/>
                       </RewardForTouchingBlockType>
                       <RewardForSendingCommand reward="-1"/>
                       <AgentQuitFromTouchingBlockType>
